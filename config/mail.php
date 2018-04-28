f2bffe85f1d79f73fbf58e0e28d418720cd6255d
Sargis <sargis.mkoyan97@gmail.com>
2018-04-29 00:41:04 +0400

    update `from` field of mail config

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 's.mkoyan18@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Sargis'),
     ],
 
     /*
