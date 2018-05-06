663213fc83a39093854ba2cc2289a9b92305eb89
Sargis <sargis.mkoyan97@gmail.com>
2018-05-06 18:11:35 +0400

    create `TagsController`

---
+++ app/Http/Controllers/TagsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Tag;
+
+class TagsController extends Controller
+{
+    public function index(Tag $tag)
+    {
+        $posts = $tag->posts;
+
+        return view('posts.index')
+            ->with('posts', $posts);
+    }
+}
