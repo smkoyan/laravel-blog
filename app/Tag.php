4b59d71c2811e1e823c39fe5e2cc80cbb3a09d4c
Sargis <sargis.mkoyan97@gmail.com>
2018-05-06 18:11:13 +0400

    set `Tag` model route key name to `name`

--- app/Tag.php
+++ app/Tag.php
@@ -9,4 +9,9 @@ class Tag extends Model
     public function posts() {
         return $this->belongsToMany(Post::class);
     }
+
+    public function getRouteKeyName()
+    {
+        return 'name';
+    }
 }
